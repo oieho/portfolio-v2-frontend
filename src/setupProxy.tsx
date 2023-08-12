5cae8ae6aa2848a83c586d6c5a44f680fe0c77c5
user <oiehomail@gmail.com>
2023-08-12 10:14:47 +0900

    no message

--- src/setupProxy.tsx
+++ src/setupProxy.tsx
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = (app) => {
   app.use(
-    '/proxy',
+    '/boards',
     createProxyMiddleware({
       target: 'https://portfolio-v2-backend:8080',
       changeOrigin: true,
