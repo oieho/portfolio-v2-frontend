fcbfcfe1d18ac6bfd8f0f2447709b5502bb499b2
Laptop\User <oiehomail@gmail.com>
2023-08-14 20:20:19 +0900

    no message

--- .history/src/index_20230314180058.tsx
+++ .history/src/index_20230314180058.tsx
@@ -47,7 +47,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <Provider store={store}>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <HelmetProvider>
         <App />
       </HelmetProvider>
