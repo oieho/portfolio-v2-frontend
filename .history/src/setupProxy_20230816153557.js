ba2102a9059546b12c283f709701dcc9b046db71
Laptop\User <oiehomail@gmail.com>
2023-08-16 15:40:59 +0900

    no message

--- .history/src/setupProxy_20230816153557.js
+++ .history/src/setupProxy_20230816153557.js
@@ -7,7 +7,6 @@ module.exports = function (app) {
       target:
         'https://port-0-portfolio-v2-backend-3prof2lll3bfr1i.sel3.cloudtype.app',
       changeOrigin: true,
-      withCredentials: true,
     }),
   );
 };
