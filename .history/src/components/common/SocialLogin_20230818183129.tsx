af8ec4f2429e079fc47aeb86371a5c4150e0e8af
Laptop\User <oiehomail@gmail.com>
2023-08-18 18:37:59 +0900

    no message

--- .history/src/components/common/SocialLogin_20230818183129.tsx
+++ .history/src/components/common/SocialLogin_20230818183129.tsx
@@ -4,11 +4,10 @@ import { checkMyInfo } from '../../modules/auth';
 import { useDispatch } from 'react-redux';
 import client from '../../lib/api/client';
 import Cookies from 'js-cookie';
-import { useAppDispatch } from '../../index';
+
 const SocialLogin = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const dispatch2 = useAppDispatch();
   const [searchParams, setSearchParams] = useSearchParams();
   const accessToken = searchParams.get('accessToken');
   const socialRefreshToken = searchParams.get('refreshToken');
@@ -16,18 +15,11 @@ const SocialLogin = () => {
   client.defaults.headers.common.authorization = `Bearer ${accessToken}`;
   useLayoutEffect(() => {
     console.log('진입');
-    dispatch2(checkMyInfo(true));
+    dispatch(checkMyInfo(true));
     Cookies.set('accessToken', accessToken as string);
     Cookies.set('refreshToken', socialRefreshToken as string);
     setSearchParams(undefined);
-  }, [
-    navigate,
-    setSearchParams,
-    accessToken,
-    socialRefreshToken,
-    dispatch,
-    dispatch2,
-  ]);
+  }, [navigate, setSearchParams, accessToken, socialRefreshToken, dispatch]);
   return <></>;
 };
 export default SocialLogin;
