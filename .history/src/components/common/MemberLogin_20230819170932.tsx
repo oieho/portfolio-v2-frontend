ce4449b066dbd17df9f55df1806e9f0ccb6db235
Laptop\User <oiehomail@gmail.com>
2023-08-19 17:13:16 +0900

    no message

--- .history/src/components/common/MemberLogin_20230819170932.tsx
+++ .history/src/components/common/MemberLogin_20230819170932.tsx
@@ -544,8 +544,8 @@ const MemberLogin = ({
           <AlignBtn>
             <KakaoBtnWrap
               onClick={() =>
-                (window.parent.location.href =
-                  'https://port-0-portfolio-v2-backend-3prof2lll3bfr1i.sel3.cloudtype.app/oauth2/authorization/kakao?redirect_uri=https://oieho.netlify.app/#/socialLogin')
+                (window.location.href =
+                  'https://port-0-portfolio-v2-backend-3prof2lll3bfr1i.sel3.cloudtype.app/oauth2/authorization/kakao?redirect_uri=https://oieho.netlify.app/socialLogin')
               }
               onMouseOver={() => setKakaoBtnHover(true)}
               onMouseOut={() => setKakaoBtnHover(kakaoBtnHover)}
