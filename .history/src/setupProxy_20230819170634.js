06007449839f9a7cea6a0837016ddb3cd7e6106a
oieho <oiehomail@gmail.com>
2023-08-25 00:39:14 +0900

    no message

--- .history/src/setupProxy_20230819170634.js
+++ .history/src/setupProxy_20230819170634.js
@@ -3,8 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
   app.use(
     createProxyMiddleware('/#/socialLogin', {
-      target:
-        'https://port-0-portfolio-v2-backend-3prof2lll3bfr1i.sel3.cloudtype.app',
+      target: 'https://portfolio-v2-backend.railway.internal',
       changeOrigin: true,
     }),
   );
