84ba8d6b64808dfb94f73b14da1af446fb18b102
oieho <oiehomail@gmail.com>
2023-08-25 17:37:54 +0900

    no message

--- src/setupProxy.js
+++
@@ -1,9 +0,0 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-module.exports = function (app) {
-  app.use(
-    createProxyMiddleware('/*', {
-      target: 'https://portfolio-v2-backend-production.up.railway.app',
-    }),
-  );
-};
