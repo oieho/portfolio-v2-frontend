709342cea2aec388564fe985e59751d42054dd72
oieho <oiehomail@gmail.com>
2023-08-28 20:28:51 +0900

    no message

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
   app.use(
     createProxyMiddleware('/*', {
-      target: 'https://portfolio-v2-backend.railway.internal',
+      target: 'http://portfolio-v2-backend-production.up.railway.app',
     }),
   );
 };
