a16c66db1280ae09d7db39e228fd68295bf77f71
oieho <oiehomail@gmail.com>
2023-08-28 20:39:36 +0900

    no message

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
   app.use(
     createProxyMiddleware('/*', {
-      target: 'http://portfolio-v2-backend-production.up.railway.app',
+      target: 'https://portfolio-v2-backend-production.up.railway.app',
       pathRewrite: {
         '^/socialLogin': '', // Remove the "/socialLogin" part from the path
       },
