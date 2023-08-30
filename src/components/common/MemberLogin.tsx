43afe19f8d1476369c9b425ae860869ba7f34e4b
oieho <oiehomail@gmail.com>
2023-08-30 13:59:38 +0900

    no message

--- src/components/common/MemberLogin.tsx
+++ src/components/common/MemberLogin.tsx
@@ -545,7 +545,7 @@ const MemberLogin = ({
             <KakaoBtnWrap
               onClick={() =>
                 (window.location.href =
-                  'http://portfolio-v2-backend:8088/oauth2/authorization/kakao?redirect_uri=http://portfolio-v2-frontend:80/#/socialLogin')
+                  'https://port-0-portfolio-v2-backend-3prof2lll3bfr1i.sel3.cloudtype.app/oauth2/authorization/kakao?redirect_uri=http://portfolio-v2-frontend:80/#/socialLogin')
               }
               onMouseOver={() => setKakaoBtnHover(true)}
               onMouseOut={() => setKakaoBtnHover(kakaoBtnHover)}
