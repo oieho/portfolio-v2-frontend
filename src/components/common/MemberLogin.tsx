4e26d195489714cb7d9d709136b66c54d13f2319
oieho <oiehomail@gmail.com>
2023-08-31 17:37:09 +0900

    no message

--- src/components/common/MemberLogin.tsx
+++ src/components/common/MemberLogin.tsx
@@ -545,7 +545,7 @@ const MemberLogin = ({
             <KakaoBtnWrap
               onClick={() =>
                 (window.location.href =
-                  'https://port-0-portfolio-v2-backend-3prof2lll3bfr1i.sel3.cloudtype.app/oauth2/authorization/kakao?redirect_uri=https://web-portfolio-v2-frontend-3prof2lll3bfr1i.sel3.cloudtype.app/socialLogin')
+                  'http://localhost:8088/oauth2/authorization/kakao?redirect_uri=http://localhost:3000/socialLogin')
               }
               onMouseOver={() => setKakaoBtnHover(true)}
               onMouseOut={() => setKakaoBtnHover(kakaoBtnHover)}
@@ -560,7 +560,7 @@ const MemberLogin = ({
             <NaverBtnWrap
               onClick={() =>
                 (window.location.href =
-                  'http://portfolio-v2-backend:8088/oauth2/authorization/naver?redirect_uri=https://oieho.xyz/socialLogin')
+                  'http://port-0-portfolio-v2-backend-3prof2lll3bfr1i.sel3.cloudtype.app/oauth2/authorization/naver?redirect_uri=http://web-portfolio-v2-frontend-3prof2lll3bfr1i.sel3.cloudtype.app/socialLogin')
               }
               onMouseOver={() => setNaverBtnHover(true)}
               onMouseOut={() => setNaverBtnHover(naverBtnHover)}
@@ -575,7 +575,7 @@ const MemberLogin = ({
             <GoogleBtnWrap
               onClick={() =>
                 (window.location.href =
-                  'https://portfolio-v2-backend:8088/oauth2/authorization/google?redirect_uri=https://oieho.xyz/socialLogin')
+                  'http://localhost:8088/oauth2/authorization/google?redirect_uri=http://localhost:3000/socialLogin')
               }
               onMouseOver={() => setGoogleBtnHover(true)}
               onMouseOut={() => setGoogleBtnHover(googleBtnHover)}
