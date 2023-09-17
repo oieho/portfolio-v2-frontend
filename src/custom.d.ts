c32eb69c5f35998b4dcc4e0d3b9ae0535818209b
oieho <oiehomail@gmail.com>
2023-09-17 13:25:20 +0900

    no message

--- src/custom.d.ts
+++
@@ -1,2 +0,0 @@
-declare module 'codemirror';
-declare module 'katex';
--- src/modules/mobxStore.ts
+++
@@ -1,5 +0,0 @@
-import modifyContent from './modifyContent';
-import deliveryImgInfoOnWriting from './deliveryImgInfoOnWriting';
-
-const store = { modifyContent, deliveryImgInfoOnWriting };
-export default store;
