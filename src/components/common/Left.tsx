9dba4cae5454acd2af3450c9fcd5e36660968fc6
oieho <oiehomail@gmail.com>
2023-09-22 20:26:43 +0900

    no message

--- src/components/common/Left.tsx
+++ src/components/common/Left.tsx
@@ -28,8 +28,9 @@ const Logo = styled.span`
   position: absolute;
   width: 100%;
   height: 3.6rem;
-  top: -1.063rem;
-  left: 0.1rem;
+  top: -1.25rem;
+  left: 3.27rem;
+  transform: rotate(45deg);
   z-index: 4;
   animation: mainLogo 1.1s ease-out infinite normal;
 
