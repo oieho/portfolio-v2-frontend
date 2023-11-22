85d34645b3b6e871cdd5953d1c113f815e9a3b48
oiehomail@gmail.com <oiehomail@gmail.com>
2023-11-22 13:20:50 +0900

    로그인 유지 체크 후 로그인하여 새로고침하면 Access Token과 Refresh Token이 제거되는 문제 해결

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { HashRouter, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import rootReducer, { rootSaga } from './modules';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
